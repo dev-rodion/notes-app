88740ea171b4a1c4fa8961009bed893720bf55f5
dev-rodion <gudkov.rodion.21@gmail.com>
2022-03-15 10:27:37 +0200

    Removed useless files, added styles. Created Main and Sidebar components with main content

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+function Main() {
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input id="title" type="text" autoFocus />
+        <textarea id="body" placeholder="Write your note here..." />
+      </div>
+
+      <div className="app-main-preview">
+        <h1 className="preview-title">TITLE</h1>
+        <div className="markdown-preview">note preview</div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
